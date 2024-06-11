72df0aec15634a00b133a7b737c99a4cf0fd0a24
Khoinace <khoinace170056@fpt.edu.vn>
2024-06-11 21:04:45 +0700

    Add Header.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import "./assets/styles/stylesall.css";
+
+import { HeaderAfterLogin,Header } from './component/Header'
 
 function App() {
   const [count, setCount] = useState(0)
