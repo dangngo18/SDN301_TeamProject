df5e8c734e50c6614c6535d68e5b5a59439c11a4
Khoinace <khoinace170056@fpt.edu.vn>
2024-06-12 11:39:10 +0700

    temp commit

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
 
 export default function Footer() {
+
+  const footerlist = [
+    {
+      header: "Information Use",
+      
+    }
+  ]
+
+
   return (
-    <div>Footer</div>
+    <div className='Footer container'>
+      <div className='Footer1'>
+        <div>
+          
+        </div>
+      </div>
+      <div className='Footer2'>
+        <div>
+
+        </div>
+      </div>
+    </div>
   )
 }
