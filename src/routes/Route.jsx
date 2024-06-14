7b153d85ca28ca6f91f73631759f8615bddc7b40
Khoinace <khoinace170056@fpt.edu.vn>
2024-06-14 13:40:58 +0700

    Add ForgotPassword apge validateEmail function change check variable define way in FindEmail

--- src/routes/Route.jsx
+++ src/routes/Route.jsx
@@ -5,6 +5,8 @@ import Home from '../pages/Home'
 import Style from '../pages/Style'
 import Shop from '../pages/Shop'
 import FindEmail from '../pages/ForgotPass/FindEmail'
+import ForgotPass from '../pages/ForgotPass/ForgotPassword'
+import OTP from '../pages/ForgotPass/OTP'
 const publicRoutes = [
     {
         path: '/',
@@ -29,7 +31,15 @@ const publicRoutes = [
     {
         path: '/FindEmail',
         component: <FindEmail />
-    }
+    },
+    {
+        path: '/ForgotPass',
+        component: <ForgotPass />
+    },
+    {
+        path: '/OTP',
+        component: <OTP />
+    },
 ]
 
 export { publicRoutes }
--- src/ultils/validation.jsx
+++ src/ultils/validation.jsx
@@ -2,14 +2,23 @@
 // src/validators.js
 
 export const validatePhoneNumber = (phoneNumber) => {
-    const re = /^0\d{9}$/;
-    if (!phoneNumber) {
-      return "Phone number is required";
-    }
-    if (!re.test(phoneNumber)) {
-      return "Phone number is not valid. It should be a 10-digit number starting with 0";
-    }
-    return null;
-  };
-  
-  
+  const re = /^0\d{9}$/;
+  if (!phoneNumber) {
+    return "Phone number is required";
+  }
+  if (!re.test(phoneNumber)) {
+    return "Phone number is not valid. It should be a 10-digit number starting with 0";
+  }
+  return null;
+};
+
+export const validateEmail = (email) => {
+  const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!email) {
+    return "Email is required";
+  }
+  if (!re.test(email)) {
+    return "Email is not valid";
+  }
+}
+
